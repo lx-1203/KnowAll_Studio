c2c748f1f4a3fa491019e30e4a07cfe79b296543
KnowAll Dev <dev@knowall.local>
2026-06-28 17:41:51 +0800

    auto: 代码自动保存 2026-06-28 17:41:51

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,15 +8,13 @@ import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ConfigProvider locale={zhCN}>
-      <AntdApp>
-        <ThemeProvider>
-          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-            <App />
-          </BrowserRouter>
-        </ThemeProvider>
-      </AntdApp>
-    </ConfigProvider>
-  </React.StrictMode>
+  <ConfigProvider locale={zhCN}>
+    <AntdApp>
+      <ThemeProvider>
+        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </AntdApp>
+  </ConfigProvider>
 )
