159b5d756cc17fb0a1e813743275daed86af03d7
KnowAll Dev <dev@knowall.local>
2026-06-28 15:42:31 +0800

    auto: 代码自动保存 2026-06-28 15:42:31

--- frontend/src/game/index.ts
+++ frontend/src/game/index.ts
@@ -4,6 +4,7 @@ export { DEFAULT_GAME_CONFIG, getTileColor, getTileFontSize, getMilestoneDifficu
 export type { GameConfig, GameState, Grid, Tile, Direction, MoveResult, GameQuestion, GameQuestionType, QuestionDifficulty, QuizResult, GameStats, QuizGateState, QuizBankConfig, GameOption } from './types'
 export { useGameStore, useTiles, getGameStats } from './hooks/useGameState'
 export { useQuizGate } from './hooks/useQuizGate'
+export { useQuizGateStore } from './hooks/useQuizGateStore'
 export { QuestionBankManager } from './quiz/QuestionBankManager'
 export { QuestionScheduler } from './quiz/QuestionScheduler'
 export { initGame, move, addRandomTile, hasAvailableMoves, getMaxTile, extractTiles } from './engine/Game2048'
